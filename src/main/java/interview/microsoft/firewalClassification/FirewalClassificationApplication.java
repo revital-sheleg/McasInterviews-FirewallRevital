bfd77de2919fdecb60d1535d73919bb629f67479
Administrator <azorevi@gmail.com>
2020-03-16 12:04:55 +0200

    add new files

---
+++ src/main/java/interview/microsoft/firewalClassification/FirewalClassificationApplication.java
@@ -0,0 +1,13 @@
+package interview.microsoft.firewalClassification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirewalClassificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirewalClassificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/interview/microsoft/firewalClassification/FirewalClassificationApplicationTests.java
@@ -0,0 +1,13 @@
+package interview.microsoft.firewalClassification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirewalClassificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
